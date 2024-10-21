0b2f378f91baa5e8a0b5db5d948a9f770ed8ca60
ghada <gh.abassi01@gmail.com>
2024-10-21 19:15:34 +0100

    changeportTo3308

--- src/main/java/sesame/com/entities/Etudiant.java
+++ src/main/java/sesame/com/entities/Etudiant.java
@@ -3,8 +3,6 @@ package sesame.com.entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
